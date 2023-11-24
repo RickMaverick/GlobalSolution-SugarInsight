c0c78a3af48a294d9e8be8726ffba7c0772a823a
ricardo-temple <ricardovtemple@gmail.com>
2023-11-24 20:56:35 -0300

    adiciona membros footer
    adicionar membros da equipe no footer

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -18,6 +18,16 @@ export default function Footer(...props){
                 <div className={styles.icon}/>
                 <Link className={styles.footerTitle} href="#">LinkedIn</Link>
             </div>
+            <div className={styles.footerCategory}>
+                <h3 className={styles.footerTitle}>Membros da Equipe</h3>
+                <div className={styles.footerNames}>
+                    <p className={styles.footerNamesText}>Ricardo Temple RM99721</p>
+                    <p className={styles.footerNamesText}>Michael Bernardi RM99409</p>
+                    <p className={styles.footerNamesText}>Thabata Orbetelli RM 550268</p>
+                    <p className={styles.footerNamesText}>Yago Ruksenas RM97827</p>
+                    <p className={styles.footerNamesText}>Mateus Viana RM99530</p>
+                </div>
+            </div>
         </div>
     )
 }
